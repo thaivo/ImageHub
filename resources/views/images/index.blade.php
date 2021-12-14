0c54ee45d8531d8c9c12193a78faac97956d33e1
thaivo <thai2903@gmail.com>
2021-12-13 23:18:07 -0500

    -Added bootstrap style for a list of images.

--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -1,13 +1,37 @@
 @extends('images.layout')
 
 @section('content')
-{{--    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addImageModal">Add image</button>--}}
-<a class="btn btn-primary" href="{{route('images.create')}}">Add an image</a>
-    @foreach($images as $image)
-    <img src="{{asset('storage/'.$image->image)}}">
-    @endforeach
+    <main>
+        <div>
+            <h1>
+                Images Store
+            </h1>
+        </div>
+        <a class="btn btn-primary" href="{{route('images.create')}}">Add an image</a>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            @foreach($images as $image)
+                <div class="col">
+                    <div class="card">
+                        <img src="{{asset('storage/'.$image->image)}}" class="card-img-top">
+{{--                        <div class="card-body">--}}
+{{--                            <h2>--}}
+{{--                                card-title--}}
+{{--                            </h2>--}}
+{{--                            <p>--}}
+{{--                                description--}}
+{{--                            </p>--}}
+{{--                        </div>--}}
+                    </div>
+                </div>
 
+            @endforeach
+        </div>
+
+    </main>
+{{--    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addImageModal">Add image</button>--}}
 @endsection
+
+
 <link rel="stylesheet" href="//code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
 <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
 <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::get('/', [ImageController::class,'index'])->name('home');
 
 Route::resource('images', ImageController::class);
